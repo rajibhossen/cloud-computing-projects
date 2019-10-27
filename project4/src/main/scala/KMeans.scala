172beb6c1528cb3c5739ef4dca8b3090e7f2ca2b
Rajib Hossen <rajibcse2k10@gmail.com>
2019-10-27 00:20:14 -0500

    project 4 completed

--- project4/src/main/scala/KMeans.scala
+++ project4/src/main/scala/KMeans.scala
@@ -1,19 +1,47 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
-object KMeans {
-  type Point = (Double,Double)
 
+object KMeans {
+
+  type Point = (Double,Double)
   var centroids: Array[Point] = Array[Point]()
 
-  def main(args: Array[ String ]) {
+  def distance (p: Point, point: Point): Double ={
+
+    var distance = Math.sqrt ((p._1 - point._1) * (p._1 - point._1) + (p._2 - point._2) * (p._2 - point._2) )
+    distance
+  }
+
+  def main (args: Array[String]): Unit ={
     /* ... */
+    val conf = new SparkConf().setAppName("KMeans")
+    val sc = new SparkContext(conf)
 
-    centroids = /* read initial centroids from centroids.txt */
+    centroids = sc.textFile(args(1)).map( line => { val a = line.split(",")
+      (a(0).toDouble,a(1).toDouble)}).collect
+    var points=sc.textFile(args(0)).map(line=>{val b=line.split(",")
+      (b(0).toDouble,b(1).toDouble)})
 
-    for ( i <- 1 to 5 )
-        centroids = /* find new centroids using KMeans */
+    for(i<- 1 to 5){
+      val cs = sc.broadcast(centroids)
+      centroids = points.map { p => (cs.value.minBy(distance(p,_)), p) }
+        .groupByKey().map { case(c,pvalue)=>
+        var count=0
+        var sumX=0.0
+        var sumY=0.0
 
+        for(pval <- pvalue) {
+          count += 1
+          sumX += pval._1
+          sumY += pval._2
+        }
+        var centroidx = sumX/count
+        var centroidy = sumY/count
+        (centroidx,centroidy)
+
+      }.collect
+    }
     centroids.foreach(println)
   }
 }
